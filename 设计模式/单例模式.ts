ede18c739684d5382b6e6f8512853c4ead07e82a
SnowNightt <1006817009@qq.com>
2024-10-22 23:26:05 +0800

    feat: 单例模式

---
+++ 设计模式/单例模式.ts
@@ -0,0 +1,20 @@
+class Singleton {
+  data?: string;
+  private static instance: Singleton;
+  constructor() {
+    if (Singleton.instance) {
+      return Singleton.instance;
+    }
+    this.data = "I am the only instance";
+    Singleton.instance = this;
+  }
+  getData() {
+    return this.data;
+  }
+}
+const instance1 = new Singleton();
+const instance2 = new Singleton();
+
+console.log(instance1 === instance2); // 输出: true (两个实例相同)
+console.log(instance1.getData()); // 输出: I am the only instance
+console.log(instance2.getData()); // 输出: I am the only instance
