0b7514d7a4d0fcd258138f21c5d4d67ab9f161f0
SnowNightt <1006817009@qq.com>
2024-10-23 10:31:18 +0800

    feat: 深拷贝升级

--- 深拷贝.js
+++ 深拷贝.js
@@ -14,3 +14,33 @@ function deepclone(obj) {
   }
   return res;
 }
+
+// 进阶版
+// 解决 循环引用、无法拷贝 undefined、function ，Date 和 RegExp拷贝成普通对象
+function deepClonePro(value, hash = new WeakMap()) {
+  // undefined或null不需要拷贝
+  if (value == null) return value;
+  // 函数不需要拷贝
+  if (typeof value == "function") return value;
+  // 原始值不需要拷贝
+  if (typeof value !== "object") return value;
+  if (value instanceof RegExp) return new RegExp(value);
+  if (value instanceof Date) return new Date(value);
+  // 剩下的情况就是value是对象
+  const obj = new value.constructor(); // {} | []
+  if (hash.get(value)) {
+    return hash.get(value);
+  }
+  hash.set(value, obj);
+  for (let key in value) {
+    if (value.hasOwnProperty(key)) {
+      obj[key] = deepClonePro(value[key], hash);
+    }
+  }
+  return obj;
+}
+const person = { name: "ming", age: 24, like: { fir: "run" } };
+const obj1 = deepClonePro(person);
+person.like.fir = "fly";
+console.log(obj1.like);
+const obj2 = {name: obj2};
