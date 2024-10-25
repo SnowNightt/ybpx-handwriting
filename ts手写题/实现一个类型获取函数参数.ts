af3a30348dc0fa746be241da3b6162ef46a0aec5
SnowNightt <1006817009@qq.com>
2024-10-25 22:06:00 +0800

    feat: 实现Parameters工具类型

--- ts手写题/实现一个类型获取函数参数.ts
+++ ts手写题/实现一个类型获取函数参数.ts
@@ -1,1 +1,5 @@
 type MyParameters<T extends (...args:any[])=>any> = T extends (...args:infer U)=>any?U:never
+function fn(name:string,age:number){
+}
+type IFn = (name:string,age:number)=>void
+type args = MyParameters<IFn>
