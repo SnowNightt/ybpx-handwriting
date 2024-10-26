c5e0327c55f246544140886a356a52dcc24bf1bd
SnowNightt <1006817009@qq.com>
2024-10-26 19:31:53 +0800

    feat: 手写数组扁平

---
+++ 数组/flat.ts
@@ -0,0 +1,13 @@
+function myFlat(arr: any[], depth: number = 1) {
+  if (depth < 1) return arr.slice();
+  return arr.reduce((acc, curr) => {
+    if (Array.isArray(curr) && depth > 1) {
+      acc.push(...myFlat(curr, depth - 1));
+    } else {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+}
+const res = myFlat([1, 2, [3, 4, [6]]]);
+console.log(res);
