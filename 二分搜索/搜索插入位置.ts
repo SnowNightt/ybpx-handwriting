4ec742a72a36f6b4c8c6bb971c306beac1243ef6
SnowNightt <1006817009@qq.com>
2024-10-29 21:18:29 +0800

    feat: 力扣35

---
+++ 二分搜索/搜索插入位置.ts
@@ -0,0 +1,11 @@
+const searchInsert = function (nums: number[], target: number) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) left = mid + 1;
+    else if (nums[mid] > target) right = mid - 1;
+  }
+  return left;
+};
