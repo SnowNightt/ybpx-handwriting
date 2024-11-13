eeb80a2e2e89f35ab5bdb8fa209c68fd9a3418c9
SnowNightt <1006817009@qq.com>
2024-11-13 21:57:27 +0800

    feat: bind重写

--- bind.ts
+++ bind.ts
@@ -1,12 +1,36 @@
-function myBind(this: Function, content: any = window, ...args: any[]) {
+// function myBind(this: Function, content: any = window, ...args: any[]) {
+//   const _this = this;
+//   const fn = function (this: unknown, ...innerArgs: any[]) {
+//     const result = _this.apply(
+//       this instanceof fn ? this : content,
+//       args.concat(innerArgs)
+//     );
+//     return result;
+//   };
+//   fn.prototype = Object.create(this.prototype);
+//   return fn;
+// }
+interface Function {
+  myBind<T extends any[], R>(
+    this: (...args: T) => R,
+    ctx: any,
+    ...args: T
+  ): (...args: T) => R;
+}
+
+Function.prototype.myBind = function <T extends any[], R>(
+  this: (...args: T) => R,
+  ctx: any = window,
+  ...args: T
+): (...args: T) => R {
   const _this = this;
-  const fn = function (this: unknown, ...innerArgs: any[]) {
-    const result = _this.apply(
-      this instanceof fn ? this : content,
-      args.concat(innerArgs)
-    );
-    return result;
-  };
+
+  function fn(this: unknown, ...a: T): R {
+    // 使用类型断言告诉 TypeScript 连接后的数组类型是 T
+    return _this.apply(this instanceof fn ? this : ctx, [...args, ...a] as T);
+  }
+
+  // 保持原型链
   fn.prototype = Object.create(this.prototype);
   return fn;
-}
+};
